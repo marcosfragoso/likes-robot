4c2bf1611063561c691419104fce5732e20310a8
Marcos <fragoso.ufcg@gmail.com>
2023-05-19 18:44:19 -0300

    atualiza projeto

--- bot_insta.py
+++ bot_insta.py
@@ -1,17 +1,20 @@
-#Inserir o link da página do usuário que se deseja curtir. Ex: "https://www.instagram.com/cordeiro/" e apertar enter
 import webbrowser, pyautogui
 from time import sleep
-webbrowser.open('https://www.instagram.com/cristiano/')
+
+usuario = 'cristiano'
+qntd_fotos = 10
+
+webbrowser.open(f'https://www.instagram.com/{usuario}/')
 sleep(10)
-#Clicar na publicação mais recente
+
 pyautogui.click(787,733, duration=1.5)
 sleep(2)
-#Colocar no range quantas fotos deseja curtir
-for video in range(10):
-    #Curtir a foto caso não esteja curtida
+
+for video in range(qntd_fotos):
+    
     pyautogui.doubleClick(714,539)
     sleep(1)
-    #Passar a foto caso esteja curtida
+    
     pyautogui.press('right')
     sleep(3.5)
 
